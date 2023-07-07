afb7dbe25b198c1f3d8e72fcea26cf7b86f392eb
TaeHyun <kt9811@naver.com>
2023-07-07 17:09:38 +0900

    pikatest

--- src/pages/pika.js
+++ src/pages/pika.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function pika() {
   return (
+    <>
     <div>pika</div>
+    <div>pika231</div>
+    <div>pika1</div>
+    <h1>123124123</h1>
+    </>
   )
 }
