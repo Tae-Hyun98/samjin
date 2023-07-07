72055b720986450727cdf0f52ffeab1dce6ef94a
woojin <kevin2205@naver.com>
2023-07-07 15:44:56 +0900

    권태현피카츄

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>권태현 피카츄배구 개못함</h1>
       <h2>123123123</h2>
       <Outlet/>
     </div>
